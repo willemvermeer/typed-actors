bf5e21b2621584f6ee70af0dde4556564b157100
Willem Vermeer <willem.vermeer@gmail.com>
2018-09-11 21:22:44 +0200

    initial support for manager/handler pattern and added a cookie

--- src/main/scala/com/example/cb/Api.scala
+++ src/main/scala/com/example/cb/Api.scala
@@ -1,24 +1,24 @@
 package com.example.cb
 
-import akka.actor.typed.{ ActorRef, Behavior }
+import java.util.UUID
+
+import akka.actor.typed.scaladsl.AskPattern.Askable
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.scaladsl.adapter._
+import akka.actor.typed.{ ActorRef, Behavior }
 import akka.actor.{ ActorSystem, Scheduler }
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.Http.ServerBinding
-import akka.http.scaladsl.server.Directives.{ complete, get, pathEndOrSingleSlash }
+import akka.http.scaladsl.model.headers.HttpCookie
+import akka.http.scaladsl.server.Directives
 import akka.stream.ActorMaterializer
+import akka.util.Timeout
 import com.example.cb.LoginManager.{ Login, LoginCommand, Response }
 import com.typesafe.config.Config
-import akka.actor.typed.scaladsl.AskPattern.Askable
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.server.Directives
-import akka.util.Timeout
-import spray.json.DefaultJsonProtocol
 
 import scala.concurrent.Future
-import scala.util.{ Failure, Success }
 import scala.concurrent.duration._
+import scala.util.{ Failure, Success }
 
 object Api extends JsonSupport {
 
@@ -60,13 +60,23 @@ object Api extends JsonSupport {
     }
   }
 
+  val cookieName = "session"
+
   def route(config: Config, manager: ActorRef[LoginCommand])(implicit askTimeout: Timeout, scheduler: Scheduler) = {
+    import Directives._
     pathEndOrSingleSlash {
       get {
-        //        complete(Response("willem"))
-        complete {
-          val future: Future[Response] = manager ? (ref => Login("123", ref))
-          future.mapTo[Response]
+        optionalCookie(cookieName) {
+          case Some(cookie) =>
+            complete(s"Your session id = ${cookie.value}")
+          case None =>
+            val sessionId = UUID.randomUUID().toString
+            setCookie(HttpCookie("session", value = sessionId)) {
+              complete {
+                val future: Future[Response] = manager ? (ref => Login(sessionId, ref))
+                future.mapTo[Response]
+              }
+            }
         }
       }
     }
--- src/main/scala/com/example/cb/LoginManager.scala
+++ src/main/scala/com/example/cb/LoginManager.scala
@@ -3,7 +3,7 @@ package com.example.cb
 import akka.actor.typed.{ ActorRef, Behavior }
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import com.example.cb.LoginManager.{ LoginCommand, Response }
+import com.example.cb.LoginManager.{ Login, LoginCommand, Response }
 import com.typesafe.config.Config
 import spray.json.DefaultJsonProtocol
 
@@ -17,18 +17,23 @@ object LoginManager extends JsonSupport {
     def id: String
   }
   case class Login(id: String, ref: ActorRef[Response]) extends LoginCommand
+  case class Logout(id: String, ref: ActorRef[Response]) extends LoginCommand
 
   case class Response(txt: String)
 
-  def apply(config: Config): Behavior[LoginCommand] = {
+  def apply(config: Config, sessions: Map[String, ActorRef[LoginCommand]] = Map.empty): Behavior[LoginCommand] = {
     Behaviors.setup[LoginCommand] { context =>
-
       Behaviors.receiveMessage[LoginCommand] {
         case command: Login =>
-          val msg = s"Starting session for ${command.id}"
-          println(msg)
-          command.ref ! Response(msg)
-          Behaviors.same
+          sessions.get(command.id) match {
+            case Some(s) =>
+              s ! command
+              Behaviors.same
+            case None =>
+              val handler = context.spawn(LoginHandler.apply(config, command.id), command.id)
+              handler ! command
+              LoginManager(config, sessions + (command.id -> handler))
+          }
       }
     }
   }
@@ -37,6 +42,10 @@ object LoginManager extends JsonSupport {
 
 object LoginHandler {
   def apply(config: Config, id: String): Behavior[LoginCommand] = {
-    Behaviors.same
+    Behaviors.receiveMessage[LoginCommand] {
+      case command: Login => println(s"Hello login ${id}")
+        command.ref ! Response("hoi")
+        Behaviors.same
+    }
   }
 }
